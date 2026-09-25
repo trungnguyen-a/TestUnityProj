9fa41563d5d527ee5c145b3026ee5bf12ff33893
trungnguyen-a <insane2806@gmail.com>
2026-09-25 19:55:07 +0700

    update player script again

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public int level = 10;
-
+    public string gender = "male";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log("Player Start");
         Debug.Log("Player level" + level);
-     
+        Debug.Log("Player gender: " + gender);
     }
 
     // Update is called once per frame
