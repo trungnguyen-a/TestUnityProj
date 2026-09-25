e80f9d7bf22140bf1abe94d88c7d4ccb65143ca0
trungnguyen-a <insane2806@gmail.com>
2026-09-25 19:54:01 +0700

    update player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public int level = 10;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log("Player Start");
+        Debug.Log("Player level" + level);
+     
     }
 
     // Update is called once per frame
